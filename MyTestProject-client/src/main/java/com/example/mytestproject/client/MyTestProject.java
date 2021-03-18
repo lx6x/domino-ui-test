b73e82405d5f8b7f2b0e7b44100dbb842a7ce8aa
Ljf <xiayong12321@163.com>
2021-03-18 14:01:59 +0800

    内容修改

--- MyTestProject-client/src/main/java/com/example/mytestproject/client/MyTestProject.java
+++ MyTestProject-client/src/main/java/com/example/mytestproject/client/MyTestProject.java
@@ -2,25 +2,25 @@ package com.example.mytestproject.client;
 
 import com.github.nalukit.nalu.plugin.elemental2.client.NaluPluginElemental2;
 import com.google.gwt.core.client.EntryPoint;
+import org.dominokit.domino.rest.DominoRestConfig;
 
 /**
- * Copyright (C) 2018 - 2019 Frank Hossfeld <frank.hossfeld@googlemail.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * TODO 算是个启动类
  */
 public class MyTestProject implements EntryPoint {
-  public void onModuleLoad() {
-    MyTestProjectApplication application = new MyTestProjectApplicationImpl();
-    application.run(new NaluPluginElemental2());
-  }
+
+    public void onModuleLoad() {
+        /*
+         * 全局服务根:
+         *   默认情况下，domino-rest 假定将其余点部署到正在运行的应用程序的同一主机和端口，因此，如果该应用程序 localhost 在端口上运行，8080则所有服务都将映射到：
+         *   http://localhost:8080/service/{path to service}
+         *   我们可以使用 DominoRestConfigclass 全局更改所有服务的默认服务根目录：如下
+         *   更改服务根目录后，所有服务都将映射到新的服务根目录
+         */
+        DominoRestConfig.initDefaults();
+        DominoRestConfig instance = DominoRestConfig.getInstance();
+        instance.setDefaultServiceRoot("http://localhost:8080");
+        MyTestProjectApplication application = new MyTestProjectApplicationImpl();
+        application.run(new NaluPluginElemental2());
+    }
 }
